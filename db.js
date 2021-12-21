85ed81293cb0008d4fd6267bbd421cbd49c56fbd
Chad-Gooch <chad.t.gooch88@gmail.com>
2021-12-21 18:17:58 -0500

    database url update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+const sequelize = new Sequelize(process.env.DATABASE_URL,
     {dialect: 'postgres',
     ssl:PerformanceObserverEntryList.env.ENVIRONMENT === 'prduction'}
     );
