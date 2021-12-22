bc7bbddc9021609b5c9dc59e6668a5667d009e21
Chad-Gooch <chad.t.gooch88@gmail.com>
2021-12-21 20:38:58 -0500

    correct type

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL,
     {dialect: 'postgres',
-    ssl:PerformanceObserverEntryList.env.ENVIRONMENT === 'prduction'}
+    ssl:PerformanceObserverEntryList.env.ENVIRONMENT === 'production'}
     );
 
 module.exports = sequelize;
